27d9194e4ca1f4b26c5e6f0f929993ed96bec7b0
Niels <nielsnickens@gmail.com>
2025-09-03 15:41:04 -0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
   // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an Episode</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.title}</h2>
+        <p>
+          {selectedEpisode.title} is about {selectedEpisode.description}
+        </p>
+      </section>
+    );
+  }
+
+  function Selection() {
+    return (
+      <section className="selection">
+        <h2>Episode Selection</h2>
+        <ul className="selection">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Episode Selector</h1>
+      </header>
+      <main>
+        <Selection />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
